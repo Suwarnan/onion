12be02fb8f810b33bc18db6d8e0de7aa51f1bc18
jan.pomikalek <jan.pomikalek@gmail.com>
2011-04-21 12:21:45 +0000

    Changed the implementation of hashdup so that it maps each of the processed files into memory (using mmap) and sorts it with quicksort. Then it is easy to spot the duplicates. This is about two times faster than reading the input into Judy1 and should typically require slightly less memory. The advantage is that the RAM requirements can be accurately estimated as the size of the largest of the processed files.

--- src/hashdup.c
+++ src/hashdup.c
@@ -7,14 +7,16 @@
  *********************************************************************/
 
 #include <errno.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/mman.h>
 #include <sys/time.h>
-#include <sys/resource.h>
-#include <Judy.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include "buzhash.h"
 #include "version.h"
 
@@ -41,17 +43,36 @@ Project home page: <http://code.google.com/p/onion/>\n",
         OUTPUT_FILE);
 }
 
-void print_progress(unsigned long int processed_bytes, float percent_done) {
+// taken from http://cs.wikipedia.org/wiki/Quicksort
+void quicksort(hash_t array[], long int left_begin, long int right_begin) {
+    hash_t pm = array[(left_begin + right_begin) / 2];
+    long int left_index, right_index;
+    left_index = left_begin;
+    right_index = right_begin;
+    do {
+        while (array[left_index] < pm)
+            left_index++;
+        while (array[right_index] > pm)
+            right_index--;
+        if (left_index <= right_index) {
+            hash_t value = array[left_index];
+            array[left_index] = array[right_index];
+            array[right_index] = value;
+            left_index++;
+            right_index--;
+        }
+    } while (left_index < right_index);
+    if (right_index > left_begin)
+        quicksort(array, left_begin, right_index);
+    if (left_index < right_begin)
+        quicksort(array, left_index, right_begin);
+}
+
+void print_progress(int processed_files, int total_files) {
     time_t now;
-    struct rusage usage;
     time(&now);
-    getrusage(RUSAGE_SELF, &usage);
-    fprintf(stderr, "[%.24s] hashdup: %6li MB processed", ctime(&now),
-            processed_bytes / (1024 * 1024));
-    if (percent_done >= 0)
-        fprintf(stderr, " (%6.2f%%)", percent_done);
-    fprintf(stderr, "\t%6li MB RAM used", usage.ru_maxrss / 1024);
-    fprintf(stderr, "\n");
+    fprintf(stderr, "[%.24s] hashdup: %i / %i files processed\n", ctime(&now),
+            processed_files, total_files);
 }
 
 int main(int argc, char **argv) {
@@ -92,13 +113,6 @@ int main(int argc, char **argv) {
         return 1;
     }
 
-    // judy
-    int judy_rc;
-    Pvoid_t judy1 = (Pvoid_t) NULL;
-    Pvoid_t judy2 = (Pvoid_t) NULL;
-
-    // other variables
-    unsigned long int processed_bytes = 0;
     int input_files_count = argc - optind;
 
     // for all input files
@@ -106,62 +120,51 @@ int main(int argc, char **argv) {
     for (i=optind; i<argc; i++) {
         // open file
         char* filename = argv[i];
-        errno = 0;
-        FILE* input_fp = fopen(filename, "r");
-        if (errno != 0) {
+        int input_fd = open(filename, O_RDONLY);
+        if (input_fd == -1) {
             fprintf(stderr, "Unable to open %s for reading.\n", filename);
             return 1;
         }
 
-        // estimate total input size
-        fseek(input_fp, 0L, SEEK_END);
-        unsigned long int file_size = ftell(input_fp);
-        fseek(input_fp, 0L, SEEK_SET);
-        int processed_files = i - optind;
-        int est_input_size;
-        if (i == argc - 1) {
-            // processing the last file
-            est_input_size = processed_bytes + file_size;
-        }
-        else {
-            est_input_size = (processed_bytes + file_size) / 
-                    (processed_files + 1) * input_files_count;
-        }
+        // determine file size
+        unsigned long int file_size = lseek(input_fd, 0L, SEEK_END);
+        lseek(input_fd, 0L, SEEK_SET);
 
-        // reset judy
-        J1FA(judy_rc, judy1);
-        J1FA(judy_rc, judy2);
+        // map hashes into memory
+        hash_t* hashes = NULL;
+        hashes = (hash_t*) mmap(hashes, file_size, PROT_READ | PROT_WRITE,
+                MAP_PRIVATE, input_fd, 0);
 
-        // for each hash in the file
+        // sort hashes
+        unsigned long int hash_count = file_size / sizeof(hash_t);
+        quicksort(hashes, 0, hash_count-1);
+
+        // send duplicate hashes to the output
+        int written = 0;
+        hash_t prev_hash = hashes[0];
         hash_t hash;
-        while (fread(&hash, sizeof(hash), 1, input_fp)) {
-            J1S(judy_rc, judy1, hash);
-            if (!judy_rc) {
-                // if found in judy1, test judy2
-                J1S(judy_rc, judy2, hash);
-                if (judy_rc) {
-                    // if not found in judy2, send to output
-                    // (duplicate encountered for the first time)
+        unsigned long int j;
+        for (j=1; j<hash_count; j++) {
+            hash = hashes[j];
+            if (hash == prev_hash) {
+                if (!written) {
                     fwrite(&hash, sizeof(hash), 1, output_fp);
+                    written = 1;
                 }
             }
-            processed_bytes+= sizeof(hash);
-
-            // print progress information
-            if (!Quiet && processed_bytes % (10000000 * sizeof(hash)) == 0) {
-                float percent_done = -1;
-                if (est_input_size > 0)
-                    percent_done = 100.0 * processed_bytes / est_input_size;
-                print_progress(processed_bytes, percent_done);
+            else {
+                written = 0;
             }
+            prev_hash = hash;
         }
 
-        fclose(input_fp);
-    }
+        munmap(hashes, file_size);
+        close(input_fd);
 
-    // print progress information
-    if (!Quiet)
-        print_progress(processed_bytes, 100);
+        // print progress information
+        if (!Quiet)
+            print_progress(i - optind + 1, input_files_count);
+    }
 
     fclose(output_fp);
 
